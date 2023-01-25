3363ee0b8325fb4a247c6b684a1af8d047ff502c
Jesse Bernoudy <jbernoudy@salesforce.com>
2023-01-25 08:09:34 -0800

    "Updated app"

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,69 +1,67 @@
 import * as React from 'react';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
-function App() {
-  return (
+const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+  return(
     <div>
       <h1>My Hacker Stories</h1>
-
       <Search />
-
       <hr />
-
-      <List list={list} />
+      <List list={stories} />
     </div>
   );
-}
+};
 
-function List({list}) {
-  return (
-    <ul>
-      {list.map(function (item) {
-        return (
-          <Item item={item}/>
-        );
-      })}
-    </ul>
-  );
-}
+const List = ({ list }) => (
+  <ul>
+    {list.map((item) =>
+      (<Item item={item} />)
+    )}
+  </ul>
+);
 
-function Search() {
+const Search = () => {
+  const handleChange = (event) => {
+    console.log(event);
+    console.log(event.target.value);
+  };
+  const handleBlur = (event) => {
+    console.log(event);
+    console.log(event.target.value);
+  };
   return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <input id="search" type="text" onChange={handleChange} onBlur={handleBlur} />
     </div>
   );
-}
+};
 
-function Item({item}) {
-  return (
-      <li key={item.objectID}>
-      <span>
-        <a href={item.url}>{item.title}</a> 
-      </span>
-      <span> {item.author}, </span>
-      <span>{item.num_comments}, </span>
-      <span>{item.points}</span>
-    </li>
-  )
-}
+const Item = ({ item }) => (
+  <li key={item.objectID}>
+    <span>
+      <a href={item.url}>{item.title}</a>
+    </span>
+    <span> {item.author}, </span>
+    <span>{item.num_comments}, </span>
+    <span>{item.points}</span>
+  </li>
+);
 export default App;
